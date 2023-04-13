fddc17ba6e0b1cb7b0772b1cb325a45fbbe56753
Aditya Padia <aditya.padia@sagadigits.com>
2023-04-13 20:09:10 +0800

    Initial Commit

---
+++ BabyCryDetector/BabyCryDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyCryDetectorApp.swift
+//  BabyCryDetector
+//
+//  Created by Aditya Padia on 13/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct BabyCryDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
